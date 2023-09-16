cbca6498de3b9342112fe2427adada61bd460c17
Anurag320 <84263527+Anurag320@users.noreply.github.com>
2023-09-16 06:56:55 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = 27881923
 api_hash = "79abda5e46a51fc0dce1313f2548ce19"
-bot_token = "6021483567:AAHotIMmLBQDJnYTAPxVpJEM_lanCuL7koU"
+bot_token = "6000596785:AAGEnr6Ndu_-t1ZnBCbopQACCevSPof3tfY"
 
